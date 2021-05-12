94b089e020d2b68cc8ed26641b33590b2160626e
andreicismas <andrei_cismas@yahoo.it>
2021-05-12 14:52:22 +0200

    add v-for in html

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const app = new Vue({
     el: '#app',
     data: {
-        
+        listAxios:[],
+        generinclud:[],
     },
+    mounted() {
+
+            // chiamata api
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(resp => {
+                this.listAxios = resp.data.response
+                console.log(this.listAxios)
+                
+                // recupero il genere musica
+                this.listAxios.forEach(disco => {
+                    if(!this.generinclud.includes(disco.genre)) {
+                        this.generinclud.push(disco.genre);
+                        console.log(this.generinclud);
+                    }
+                });
+            });
+       
+
+    }
 });
